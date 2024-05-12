a8e1f21ce19ddf7b518e594721465c2b84db204a
shashank <142093874+shashank-katiyar@users.noreply.github.com>
2024-05-12 22:20:33 +0530

    Format files according to Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 import { ThemeProvider } from "@/components/ui/theme-provider"
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "LeetcodeBoard",
   description: "Online whiteboarding for leetcode problems",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider></body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
 import { Button } from "@/components/ui/button"
-import { ModeToggle } from "@/components/ui/mode-toogle";
+import { ModeToggle } from "@/components/ui/mode-toogle"
 
 export default function Home() {
   return (
-    
-<div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-  <img src="/img/beams.jpg" alt="" className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
-  <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-  <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-    <div className="mx-auto max-w-md">
-      <div className="flex space-x-4">
-      <Button>Hello</Button>
-      <ModeToggle/>
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
+      <img
+        src="/img/beams.jpg"
+        alt=""
+        className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
+        width="1308"
+      />
+      <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <div className="mx-auto max-w-md">
+          <div className="flex space-x-4">
+            <Button>Hello</Button>
+            <ModeToggle />
+          </div>
+        </div>
       </div>
-     
-      </div>
-  </div>
-</div>
-  );
+    </div>
+  )
 }
