cd7410b0170d007a402df982f33e0d8c6f504acd
shashank <142093874+shashank-katiyar@users.noreply.github.com>
2024-05-12 23:20:02 +0530

    adding nav bar

---
+++ components/main-nav.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+
+
+export function MainNav() {
+  const pathname = usePathname()
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+
+        <span className="hidden font-bold sm:inline-block">
+          {siteConfig.name}
+        </span>
+      </Link>
+      <nav className="flex items-center gap-4 text-sm lg:gap-6">
+        <Link
+          href="/docs"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/docs" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Page 1
+        </Link>
+        <Link
+          href="/docs/components"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/docs/components")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Page 2
+        </Link>
+        <Link
+          href="/themes"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/themes")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Page 3
+        </Link>
+        <Link
+          href="/examples"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/examples")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          page 4
+        </Link>
+        <Link
+          href="/blocks"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/blocks")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Page 5
+        </Link>
+
+      </nav>
+    </div>
+  )
+}
